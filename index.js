e0c79b8f836059f01fffa956a8f1a5684931c564
downthecrop <bryce@mathx.ca>
2021-09-11 20:00:07 -0700

    aspectratio

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var inFile
 
+function aspectRatio(w,h){
+  let nH = document.getElementById("myImage").naturalHeight
+  let nW = document.getElementById("myImage").naturalWidth
+  let ratio = nW/nH
+  return([parseInt(h*ratio),parseInt(w/ratio)])  
+}
+
 document.addEventListener('drop', (event) => {
     event.preventDefault();
     event.stopPropagation();
@@ -36,12 +43,13 @@ document.getElementById('file_picker').onchange = function() {
   for (let i = 0; i < f.length; i += 1){
     let images = new Image();
     images.onload = () => {
-     console.log("Image Size", images.width, images.height)
-     document.getElementById("width").value = images.width
-     document.getElementById("height").value = images.height
+     console.log("Image Size", images.naturalWidth, images.naturalHeight)
+     document.getElementById("width").value = images.naturalWidth
+     document.getElementById("height").value = images.naturalHeight
     }
     images.onerror = () => result(true);
     images.src = f[i].path
+    images.id = "myImage"
     inFile = f[i].path
 
     // Add image to the DOM just so we can get its width and height
@@ -52,14 +60,25 @@ document.getElementById('file_picker').onchange = function() {
   }
 };
 
+document.getElementById("width").onchange = function(){
+  let width = document.getElementById("width")
+  let d = aspectRatio(width.value,1)
+  console.log(d)
+  height.value = d[1]
+}
 
+document.getElementById("height").onchange = function(){
+  let height = document.getElementById("height")
+  let d = aspectRatio(1,height.value)
+  console.log(d)
+  width.value = d[0]
+}
 
 document.getElementById('start').onclick = function(){
     let w = document.getElementById("width").value;
     let h = document.getElementById("height").value;
     let fps = document.getElementById("fps").value;
-
-    let command = `-i ${inFile} -filter_complex fps=${fps},scale=${w}:-1:flags=fast_bilinear,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=sierra2_4a output.gif`
+    let command = `-i ${inFile} -filter_complex fps=${fps},scale=${w}:${h}:flags=fast_bilinear,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=sierra2_4a output.gif`
 
     let myArgs = command.split(" ");    
     const execFile = require('child_process').execFile;
